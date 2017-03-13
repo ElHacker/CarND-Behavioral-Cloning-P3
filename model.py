049c887de6fdae9f7db2e5931832c86c0b5ef72b
elhacker <braulio.vladimir.cn@gmail.com>
2017-03-12 19:49:28 -0700

    Add dropout layer and plot

--- model.py
+++ model.py
@@ -1,10 +1,12 @@
 import csv
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import sklearn
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D
+from keras.layers.core import Dropout
 from keras.layers.pooling import MaxPooling2D
 from sklearn.model_selection import train_test_split
 
@@ -17,6 +19,11 @@ with open('./data/driving_log.csv') as csvfile:
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 def generator(samples, batch_size=32):
+    """
+    Return a shuffled set of samples read from the data file.
+    :samples: the full set of samples to go through.
+    :batch_size: how big is the returned sub-set of samples array going to be.
+    """
     num_samples = len(samples)
     while 1:
         sklearn.utils.shuffle(samples)
@@ -81,6 +88,7 @@ model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation="relu"))
 model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation="relu"))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
@@ -88,10 +96,19 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit_generator(
+history_object = model.fit_generator(
         train_generator,
         samples_per_epoch=len(train_samples),
         validation_data=validation_generator,
         nb_val_samples=len(validation_samples),
         nb_epoch=epochs)
 model.save('model.h5')
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
